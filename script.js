fec5588883b225479adad9ad58c6c36908efb0b2
David Hasenson <90691386+davidhasenson@users.noreply.github.com>
2026-05-27 13:45:08 +0200

    Remove unnecessary call to loadCustomersForBooking after creating a customer

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ async function createCustomer() {
             document.getElementById("email").value = "";
             document.getElementById("phone").value = "";
 
-            loadCustomersForBooking();
+           
             return;
         }
 
